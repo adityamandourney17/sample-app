92a40a1d48d6e7a82739270a3ffd72df012f26ca
root <root@ip-172-31-33-105.ap-south-1.compute.internal>
2026-05-22 13:16:23 +0000

    code push

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
